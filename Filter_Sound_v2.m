ba6209e8c576a1a71b2312a66531b04c7c0b679f
Kushal Bakshi <52367253+kushalbakshi@users.noreply.github.com>
2021-01-12 19:40:43 -0600

    Threshold variable changes added
    Threshold over standard deviation variable added and moved to be included in user defined variables. Also added some annotations

--- Filter_Sound_v2.m
+++ Filter_Sound_v2.m
@@ -1,5 +1,6 @@
 % Generate waveforms and spectrograms after applying custom bandpass
 % filters and spectrogram window settings to audio signals.
+%% Author: Kushal Bakshi
 clear
 close all
 %% ENTER USER-DEFINED INFORMATION BELOW - file name/location, filters, etc.
@@ -11,13 +12,16 @@ FileName = '';
 FileLocation = '';
 
 % Enter the sampling rate in Hz
-fs = 96000;
+fs = 192000;
+
+% Threshold over standard deviation to look for clicks in the waveform
+threshold_over_std = 3;
 
 % Enter lower and higher frequencies you want to keep (everything outside
 % this range will be excluded). DO NOT SET THIS VERY CLOSE TO FREQUENCIES OF
 % INTEREST
 lower_freq = 1000;
-upper_freq = 45000;
+upper_freq = 90000;
 
 % Enter the sample window size and sample overlap for spectrogram
 window_size = 1024;
@@ -25,21 +29,19 @@ overlap_size = 256;
 
 %% Script for figures
 [b,a] = butter(4,[lower_freq upper_freq]/(fs/2),'bandpass');
-%[d,c] = ellip(2,3,40,[(32000/(fs/2)),(33000/(fs/2))],'stop');
+
 unfiltered_recording = audioread([FileLocation, FileName]);
 filtered_recording = filtfilt(b,a,unfiltered_recording);
-%filtered_recording = filtfilt(d,c,filtered_recording);
-filtered_recording = filtered_recording(:,1);
-threshold = std(filtered_recording)*2;
-[pks,locs] = findpeaks(filtered_recording, 'MinPeakHeight',threshold,...
+
+filtered_recording = filtered_recording(:,1); % Only uses ch. 1
+thr = std(filtered_recording)*threshold_over_std;
+[pks,locs] = findpeaks(filtered_recording, 'MinPeakHeight',thr,...
     'MinPeakDistance',(275*fs)/1000);
-SNR = 85;
 
 figure;
 
 ax1 = subplot(2,1,1);
-spectrogram(filtered_recording,window_size,overlap_size,window_size,fs,'yaxis',...
-    'MinThreshold',-SNR);
+spectrogram(filtered_recording,window_size,overlap_size,window_size,fs,'yaxis');
 colormap jet
 colorbar off
 title('Filtered Sound')
